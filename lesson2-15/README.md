4bf5b4f4b012192f09ccaf0bf8bb9da8c8e191c7
Nikolas Begetis <nmpegetis@gmail.com>
2018-12-24 21:34:05 +0200

    feat: add lesson2-15, rename lesson2-9

---
+++ lesson2-15/README.md
@@ -0,0 +1,5 @@
+# Coding Softmax
+
+Let's code the formula for the Softmax function in Python.
+
+Write a function that takes as input a list of numbers, and returns the list of values given by the softmax function.
---
+++ lesson2-15/solution2_15.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Write a function that takes as input a list of numbers, and returns
+# the list of values given by the softmax function.
+
+
+def softmax(L):
+    expL = np.exp(L)
+    return np.divide(expL, expL.sum())
+
+
+# e.g. [1, 2, 3, 4]
+arr1 = [1, 2, 3, 4]
+out = softmax(arr1)
+print("\nOutput array : \n", out)
